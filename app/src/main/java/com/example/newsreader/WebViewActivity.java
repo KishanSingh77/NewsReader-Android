f9473384edc762b68179a09c94b23079df369ce4
Amit <amit@Amits-MacBook-Pro.local>
2019-10-13 11:44:36 -0400

    web view changes and cleartextTraffic

--- app/src/main/java/com/example/newsreader/WebViewActivity.java
+++ app/src/main/java/com/example/newsreader/WebViewActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class WebViewActivity extends AppCompatActivity {
     WebView webView;
@@ -13,6 +14,8 @@ public class WebViewActivity extends AppCompatActivity {
         setContentView(R.layout.activity_web_view);
         webView=findViewById(R.id.webView);
         String url= (String) getIntent().getExtras().getCharSequence("url");
+        webView.setWebViewClient(new WebViewClient());
+       // webView.loadUrl(url);
         webView.loadUrl(url);
 
     }
